1b5098644f7578c22ae88ee3f7c8c987d95176c5
Isaac Uduehi <osayi4jesus@gmail.com>
2022-08-29 12:21:19 +0100

    Icons

--- src/components/Post/components/Footer.js
+++ src/components/Post/components/Footer.js
@@ -1,9 +1,23 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 
+import ADIcon from 'react-native-vector-icons/AntDesign';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
+import IonicIcon from 'react-native-vector-icons/Ionicons';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
+
 const Footer = ({likesCount, caption, timePosted}) => {
   return (
     <View style={styles.container}>
+      <View style={styles.iconContainer}>
+        <View style={styles.leftIcons}>
+          <ADIcon name="hearto" size={25} color={'#545454'} />
+          <FontistoIcon name="comment" size={23} color={'#545454'} />
+          <IonicIcon name="paper-plane-outline" size={26} color={'#545454'} />
+        </View>
+        <FAIcon name="bookmark-o" size={25} color={'#545454'} />
+      </View>
+
       <Text style={styles.likes}>{likesCount} Likes</Text>
       <Text style={styles.caption}>{caption}</Text>
       <Text style={styles.timePosted}>{timePosted}</Text>
@@ -25,7 +39,17 @@ const styles = StyleSheet.create({
   timePosted: {
     color: '#8c8c8c',
     margin: 3,
-    fontSize: 14
+    fontSize: 14,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+  },
+  leftIcons: {
+    flexDirection: 'row',
+    width: 120,
+    justifyContent: 'space-between',
   },
 });
 
