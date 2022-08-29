a56666d33555dfa20e9bef5f265b3b96d38fed82
Isaac Uduehi <osayi4jesus@gmail.com>
2022-08-30 00:48:17 +0100

    Tab Navigator

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,6 +5,7 @@ import Stories from './Stories';
 
 const data = [
   {
+    id: '1',
     user: {
       imageUri:
         'https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/287166766_1028531151370531_1833629095788263918_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=xO5sowTgL-wAX96vMnz&_nc_ht=scontent-los2-1.xx&oh=00_AT8MOGt7JvKbx51_wraNJt5M15ffDfYBnmQf5sNvjKEaaw&oe=6311AA40',
@@ -17,6 +18,7 @@ const data = [
     timePosted: '6 minutes ago',
   },
   {
+    id: '2',
     user: {
       imageUri:
         'https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/287166766_1028531151370531_1833629095788263918_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=xO5sowTgL-wAX96vMnz&_nc_ht=scontent-los2-1.xx&oh=00_AT8MOGt7JvKbx51_wraNJt5M15ffDfYBnmQf5sNvjKEaaw&oe=6311AA40',
@@ -29,6 +31,7 @@ const data = [
     timePosted: '6 minutes ago',
   },
   {
+    id: '3',
     user: {
       imageUri:
         'https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/287166766_1028531151370531_1833629095788263918_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=xO5sowTgL-wAX96vMnz&_nc_ht=scontent-los2-1.xx&oh=00_AT8MOGt7JvKbx51_wraNJt5M15ffDfYBnmQf5sNvjKEaaw&oe=6311AA40',
@@ -41,6 +44,7 @@ const data = [
     timePosted: '6 minutes ago',
   },
   {
+    id: '4',
     user: {
       imageUri:
         'https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/287166766_1028531151370531_1833629095788263918_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=xO5sowTgL-wAX96vMnz&_nc_ht=scontent-los2-1.xx&oh=00_AT8MOGt7JvKbx51_wraNJt5M15ffDfYBnmQf5sNvjKEaaw&oe=6311AA40',
@@ -53,6 +57,7 @@ const data = [
     timePosted: '6 minutes ago',
   },
   {
+    id: '5',
     user: {
       imageUri:
         'https://scontent-los2-1.xx.fbcdn.net/v/t39.30808-6/287166766_1028531151370531_1833629095788263918_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=xO5sowTgL-wAX96vMnz&_nc_ht=scontent-los2-1.xx&oh=00_AT8MOGt7JvKbx51_wraNJt5M15ffDfYBnmQf5sNvjKEaaw&oe=6311AA40',
@@ -71,6 +76,7 @@ const Feed = () => {
     <FlatList
       data={data}
       ListHeaderComponent={<Stories />}
+      keyExtractor={({id}) => id}
       renderItem={({item}) => <Post post={item} />}
     />
   );
